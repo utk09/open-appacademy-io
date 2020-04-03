4aa200a09f9d3d4d2745568934ed78ee376a0bbd
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-03 20:00:05 +0530

    added count_a program

---
+++ 1_IntroToProgramming/Loops/count_a.py
@@ -0,0 +1,19 @@
+# Write a program that counts the number of "E/e" in a string.
+
+
+def count_a(word):
+    count = 0
+    i = 0
+    while i < len(word):
+        char = word[i]
+        if char == "a" or char == "A":
+            count += 1
+        i += 1
+    print(count)
+    return count
+
+
+count_a("application")  # => 2
+count_a("bike")         # => 0
+count_a("Arthur")       # => 1
+count_a("Aardvark")     # => 3
