c63198e5a0afa70dfbca2d48c79eaee118d502ed
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-03 19:55:53 +0530

    added count e program

--- 1_IntroToProgramming/Loops/count_e.py
+++ 1_IntroToProgramming/Loops/count_e.py
@@ -1,2 +1,16 @@
 # Write a program that counts the number of "E/e" in a string.
 
+
+def count_e(word):
+    count = 0
+    i = 0
+    while i < len(word):
+        char = word[i]
+        if char == "E" or char == "e":
+            count += 1
+        i += 1
+    return count
+
+
+print("Count for 'Excellent': ", count_e("excellent"))
+print("Count for 'entrepreneurialism': ", count_e("Entrepreneurialism"))
