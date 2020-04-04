fe84629ff4ef8cb2ef5afa4fb7ab30ec2a9b454e
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-04 15:29:51 +0530

    added reverse string sol 2

--- 1_IntroToProgramming/Loops/reverse_string.py
+++ 1_IntroToProgramming/Loops/reverse_string.py
@@ -14,3 +14,23 @@ print(reverse("cat"))  # prints "tac"
 print(reverse("programming"))  # prints ""
 print(reverse("bootcamp"))   # prints "pmactoob"
 print(reverse("noon"))  # print "noon"
+
+
+# Solution 2
+
+print("---------------------------------------------")
+
+
+def reverse2(word):
+    str_value = ''
+    new_word = ''
+    for i in range(len(word)):
+        str_value = word[i]
+        new_word = str_value + new_word
+    return(new_word)
+
+
+print(reverse2("cat"))  # prints "tac"
+print(reverse2("programming"))  # prints ""
+print(reverse2("bootcamp"))   # prints "pmactoob"
+print(reverse2("noon"))  # print "noon"
