d4fad7e6ec0810a385c968fd92a2ce7f22dc6fb8
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-05 23:26:35 +0530

    added arrays folder

---
+++ 1_IntroToProgramming/Arrays/doubler.py
@@ -0,0 +1,8 @@
+# Write a method doubler(numbers) that takes an array of numbers and returns a new array where every element of the original array is multiplied by 2.
+
+
+def doubler(numbers):
+    pass
+
+
+print(doubler[1, 2, 3, 4])
