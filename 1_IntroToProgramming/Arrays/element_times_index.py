e6272a1129d2cb4b3452e029a8037b3719b0838a
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-06 11:43:25 +0530

    added modifications to element_times_index

--- 1_IntroToProgramming/Arrays/element_times_index.py
+++ 1_IntroToProgramming/Arrays/element_times_index.py
@@ -1,4 +1,6 @@
-"""Write a method element_times_index(nums) that takes in an array of numbers and returns a new array containing every number of the original array multiplied with its index."""
+"""
+Write a method element_times_index(nums) that takes in an array of numbers and returns a new array containing every number of the original array multiplied with its index.
+"""
 
 
 def element_times_index(numbers):
---
+++ 1_IntroToProgramming/Arrays/even.py
@@ -0,0 +1,3 @@
+"""
+Write a method even_nums(max) that takes in a number max and returns an array containing all even numbers from 0 to max
+"""
