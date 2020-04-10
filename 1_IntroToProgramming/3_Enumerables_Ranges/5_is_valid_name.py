d34f2c6f37b1fcc970ec447dda84c693e34dc8ca
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-10 11:26:00 +0530

    added is valid name program

--- 1_IntroToProgramming/3_Enumerables_Ranges/5_is_valid_name.py
+++ 1_IntroToProgramming/3_Enumerables_Ranges/5_is_valid_name.py
@@ -11,7 +11,24 @@ Write a method is_valid_name that takes in a string and returns a boolean indica
 
 
 def is_valid_name(string):
-    pass
+    old_name = string.split(" ")
+    name = 0
+    x = []
+    if len(old_name) < 2:
+        return False
+    else:
+        while name < len(old_name):
+            new_name = old_name[name]
+            if new_name == new_name.capitalize():
+                x.append(True)
+            else:
+                x.append(False)
+            name += 1
+        for i in x:
+            if x[i] == True:
+                return True
+            else:
+                return False
 
 
 print(is_valid_name("Kush Patel"))       # => true
