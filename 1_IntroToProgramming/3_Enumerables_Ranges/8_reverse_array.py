b3c9c362da9c6feae3c7fc5225206d3faf5ed76b
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-14 20:14:25 +0530

    added reverse array question

---
+++ 1_IntroToProgramming/3_Enumerables_Ranges/8_reverse_array.py
@@ -0,0 +1,12 @@
+"""
+Write a method rotate_array that takes in an array and a number. The method should return the array after rotating the elements the specified number of times. A single rotation takes the last element of the array and moves it to the front.
+"""
+
+
+def rotate_array(arr, num):
+    pass
+
+
+# => [ "Matthias", "Matt", "Danny", "Mashu" ]
+print(rotate_array(["Matt", "Danny", "Mashu", "Matthias"], 1))
+print(rotate_array(["a", "b", "c", "d"], 2))  # => [ "c", "d", "a", "b" ]
