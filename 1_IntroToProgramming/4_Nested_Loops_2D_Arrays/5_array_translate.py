5b3d1070fa0c2d75a1be554717d034491d051b02
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-20 15:24:14 +0530

    added array_translate & pig_latin question

---
+++ 1_IntroToProgramming/4_Nested_Loops_2D_Arrays/5_array_translate.py
@@ -0,0 +1,13 @@
+"""
+Write a method array_translate that takes in an array whose elements alternate between words and numbers. The method should return a string where each word is repeated the number of times that immediately follows in the array.
+"""
+
+
+def array_translate(array):
+    pass
+
+
+# => "CatCatDogDogDogMouse"
+print(array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]))
+
+print(array_translate(["red", 3, "blue", 1]))  # => "redredredblue"
---
+++ 1_IntroToProgramming/4_Nested_Loops_2D_Arrays/6_pig_latin_word.py
@@ -0,0 +1,17 @@
+"""
+Write a method pig_latin_word that takes in a word string and translates the word into pig latin.
+"""
+
+# Pig latin translation uses the following rules:
+# - for words that start with a vowel, add 'yay' to the end
+# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
+
+
+def pig_latin_word(word):
+    pass
+
+
+print(pig_latin_word("apple"))   # => "appleyay"
+print(pig_latin_word("eat"))     # => "eatyay"
+print(pig_latin_word("banana"))  # => "ananabay"
+print(pig_latin_word("trash"))   # => "ashtray"
