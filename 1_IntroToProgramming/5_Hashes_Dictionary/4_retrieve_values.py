4896087d119a245896e1ad6d104a156f7b516f3f
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 20:16:50 +0530

    reformatted

--- 1_IntroToProgramming/5_Hashes_Dictionary/4_retrieve_values.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/4_retrieve_values.py
@@ -1,5 +1,5 @@
 """
-Write a method retrieve_values that takes in two hashes and a key. The method should return an array containing the values from the two hashes that correspond with the given key.
+Write a method retrieve_values that takes in two dictionary's and a key. The method should return an array containing the values from the two hashes that correspond with the given key.
 """
 
 
