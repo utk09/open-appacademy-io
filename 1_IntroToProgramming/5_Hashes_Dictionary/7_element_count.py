8be228a520fea29edb14e4809e88aba176bcf630
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 20:26:23 +0530

    reformatted

--- 1_IntroToProgramming/5_Hashes_Dictionary/7_element_count.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/7_element_count.py
@@ -8,5 +8,6 @@ def element_count(arr):
 
 
 print(element_count(["a", "b", "a", "a", "b"]))  # => {"a":3, "b":2}
+
 # => {"red": 2, "blue": 1, "green": 1}
 print(element_count(["red", "red", "blue", "green"]))
