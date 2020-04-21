46d71c9403af9d89bea62c252006d3d90f4f0a25
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 17:21:01 +0530

    completed double age

--- 1_IntroToProgramming/5_Hashes_Dictionary/1_get_double_age.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/1_get_double_age.py
@@ -1,10 +1,11 @@
 """
-Write a method get_double_age that takes in a hash and returns twice the "age" value of the hash.
+Write a method get_double_age that takes in a dictionaary and returns twice the "age" value of the dictionary.
 """
 
 
-def get_double_age(hash):
-    pass
+def get_double_age(hash_in):
+    x = hash_in["age"]
+    return x*2
 
 
 print(get_double_age({"name": "App Academy", "age": 5}))  # => 10
