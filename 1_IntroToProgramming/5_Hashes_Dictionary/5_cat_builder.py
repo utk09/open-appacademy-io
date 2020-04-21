6613da463580a13e59fd6d3cd59e3bcf087b841d
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 20:12:28 +0530

    added cat_builder

--- 1_IntroToProgramming/5_Hashes_Dictionary/5_cat_builder.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/5_cat_builder.py
@@ -1,15 +1,18 @@
 """
-Write a method cat_builder that takes in a name, color, and age. The method should return a hash representing a cat with those values.
+Write a method cat_builder that takes in a name, color, and age. The method should return a dictionary representing a cat with those values.
 """
 
 
 def cat_builder(name_str, color_str, age_num):
-    pass
+    final_dict = {}
+    final_dict["name"] = name_str
+    final_dict["color"] = color_str
+    final_dict["age"] = age_num
+    return final_dict
 
 
 # => {"name":"Whiskers", "color":"orange", "age":3}
 print(cat_builder("Whiskers", "orange", 3))
-
 
 # => {"name":"Salem", "color":"black", "age":100}
 print(cat_builder("Salem", "black", 100))
