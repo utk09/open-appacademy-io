0c69ec1b7602e375c1add31b210e9ef403f1e3a2
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 23:52:00 +0530

    reformatted

--- 1_IntroToProgramming/5_Hashes_Dictionary/13_element_replace.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/13_element_replace.py
@@ -1,9 +1,9 @@
 """
-Write a method element_replace that takes in an array and a hash. The method should return a new array where elements of the original array are replaced with their corresponding values in the hash.
+Write a method element_replace that takes in an array and a dictionary. The method should return a new array where elements of the original array are replaced with their corresponding values in the dictionary.
 """
 
 
-def element_replace(arr, hash):
+def element_replace(arr, hash_in):
     pass
 
 
@@ -11,6 +11,7 @@ arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
 hash1 = {"Serena Williams": "tennis", "LeBron James": "basketball"}
 # => ["basketball", "Lionel Messi", "tennis"]
 print(element_replace(arr1, hash1))
+
 
 arr2 = ["dog", "cat", "mouse"]
 hash2 = {"dog": "bork", "cat": "meow", "duck": "quack"}
