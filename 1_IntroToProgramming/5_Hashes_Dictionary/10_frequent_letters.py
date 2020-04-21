98b14d8c34b70e1d00e20bbd0d5b0917f99b65a0
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 00:02:42 +0530

    added frequent letters

--- 1_IntroToProgramming/5_Hashes_Dictionary/10_frequent_letters.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/10_frequent_letters.py
@@ -4,10 +4,16 @@ Write a method frequent_letters that takes in a string and returns an array cont
 
 
 def frequent_letters(string):
+    repeated_words_array = []
     repeated_dict = {each_letter: string.count(
         each_letter) for each_letter in string}
+    for key, values in repeated_dict.items():
+        if values > 2:
+            repeated_words_array.append(key)
+    return repeated_words_array
 
 
 print(frequent_letters('mississippi'))  # => ["i", "s"]
 
 print(frequent_letters('bootcamp'))  # => []
+
