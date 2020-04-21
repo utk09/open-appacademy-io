1daf61c7ce064499a73348f1fbe84f141cac2d57
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-21 18:17:26 +0530

    added word length

--- 1_IntroToProgramming/5_Hashes_Dictionary/3_word_lengths.py
+++ 1_IntroToProgramming/5_Hashes_Dictionary/3_word_lengths.py
@@ -4,9 +4,14 @@ Write a method word_lengths that takes in a sentence string and returns a hash w
 
 
 def word_lengths(sentence):
-    pass
+    final_dict = {}
+    split_sentence = sentence.split(" ")
+    for each_word in split_sentence:
+        final_dict[each_word] = len(each_word)
+    return final_dict
 
 
-print(word_lengths("this is fun"))  # => {"this":4, "is":2, "fun":3}
-# => {"When":4, "in":2, "doubt,":6, "leave":5, "it":2, "out":3}
+print(word_lengths("this is fun"))  # => {"this": 4, "is": 2, "fun": 3}
+
+# => {"When": 4, "in": 2, "doubt,": 6, "leave": 5, "it": 2, "out": 3}
 print(word_lengths("When in doubt, leave it out"))
