9782d44451c45d9b3493fa9948c06100debd588d
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:23:12 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/23_consonant_cancel.py
@@ -0,0 +1,13 @@
+"""
+Write a method consonant_cancel that takes in a sentence and returns a new sentence where every word begins with it's first vowel.
+"""
+
+
+def consonant_cancel(sentence):
+    pass
+
+
+print(consonant_cancel("down the rabbit hole"))  # => "own e abbit ole"
+
+# => "iting ode is allenging"
+print(consonant_cancel("writing code is challenging"))
