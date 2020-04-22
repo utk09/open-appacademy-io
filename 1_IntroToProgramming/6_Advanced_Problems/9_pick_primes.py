ce9e29b361d47536ce9d8a3b650da4a6ba7a7bd6
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:40:28 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/9_pick_primes.py
@@ -0,0 +1,12 @@
+"""
+Write a method pick_primes that takes in an array of numbers and returns a new array containing only the prime numbers.
+"""
+
+
+def pick_primes(numbers):
+    pass
+
+
+print(pick_primes([2, 3, 4, 5, 6]))  # => [2, 3, 5]
+
+print(pick_primes([101, 20, 103, 2017]))  # => [101, 103, 2017]
