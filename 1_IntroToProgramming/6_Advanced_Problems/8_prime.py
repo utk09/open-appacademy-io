3fb864cb3564c4b73d35656e7859abec8524604e
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:38:09 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/8_prime.py
@@ -0,0 +1,15 @@
+"""
+Write a method prime_or_not that takes in a number and returns a boolean, indicating whether the number is prime. A prime number is only divisible by 1 and itself.
+"""
+
+
+def prime_or_not(num):
+    pass
+
+
+print(prime_or_not(2))  # => true
+print(prime_or_not(5))  # => true
+print(prime_or_not(11))  # => true
+print(prime_or_not(4))  # => false
+print(prime_or_not(9))  # => false
+print(prime_or_not(-5))  # => false
