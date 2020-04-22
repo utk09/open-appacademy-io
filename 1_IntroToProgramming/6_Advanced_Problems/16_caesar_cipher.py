936212d38bf6d6a4fcdfd40ad44f41cc1bcfddd5
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:00:32 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/16_caesar_cipher.py
@@ -0,0 +1,15 @@
+"""
+Write a method caesar_cipher that takes in a string and a number. The method should return a new string where every character of the original is shifted num characters in the alphabet.
+"""
+
+# Feel free to use this variable:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def caesar_cipher(string, num):
+    pass
+
+
+print(caesar_cipher("apple", 1))  # => "bqqmf"
+print(caesar_cipher("bootcamp", 2))  # => "dqqvecor"
+print(caesar_cipher("zebra", 4))  # => "difve"
