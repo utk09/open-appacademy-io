22e7d118e22feb492fde254a396958b3ca518e59
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 11:06:46 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/3_yell_sentence.py
@@ -0,0 +1,11 @@
+"""
+Write a method yell_sentence that takes in a sentence string and returns a new sentence where every word is yelled. See the examples. Use map to solve this.
+"""
+
+
+def yell_sentence(sent):
+    pass
+
+
+# => "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
+print(yell_sentence("I have a bad feeling about this"))
