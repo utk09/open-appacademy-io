4a7d987451d32b9c484838eaa2b64dbcbaef9b0e
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:21:16 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/22_anagrams.py
@@ -0,0 +1,13 @@
+"""
+Write a method anagrams_or_not that takes in two words and returns a boolean indicating whether or not the words are anagrams. Anagrams are words that contain the same characters but not necessarily in the same order. Solve this without using .sort
+"""
+
+
+def anagrams_or_not(word1, word2):
+    pass
+
+
+print(anagrams_or_not("cat", "act"))  # => true
+print(anagrams_or_not("restful", "fluster"))  # => true
+print(anagrams_or_not("cat", "dog"))  # => false
+print(anagrams_or_not("boot", "bootcamp"))  # => false
