f185f1c0f3d939f5ebdeca8e060cd072e77a5f61
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:46:45 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/12_perfect_square.py
@@ -0,0 +1,14 @@
+"""
+Write a method perfect_square_or_not that takes in a number and returns a boolean indicating whether it is a perfect square. A perfect square is a number that results from multiplying a number by itself. For example, 9 is a perfect square because 3  3 = 9, 25 is a perfect square because 5  5 = 25.
+"""
+
+
+def perfect_square_or_not(num):
+    pass
+
+
+print(perfect_square_or_not(5))  # => false
+print(perfect_square_or_not(12))  # => false
+print(perfect_square_or_not(30))  # => false
+print(perfect_square_or_not(9))  # => true
+print(perfect_square_or_not(25))  # => true
