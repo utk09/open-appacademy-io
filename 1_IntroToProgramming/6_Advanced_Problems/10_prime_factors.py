01c3110baa0085c25c2bb0809743d7d2d2bc2c5f
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:42:21 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/10_prime_factors.py
@@ -0,0 +1,12 @@
+"""
+Write a method prime_factors that takes in a number and returns an array containing all of the prime factors of the given number.
+"""
+
+
+def prime_factors(number):
+    pass
+
+
+print(prime_factors(24))  # => [2, 3]
+
+print(prime_factors(60))  # => [2, 3, 5]
