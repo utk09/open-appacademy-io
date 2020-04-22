cb6fc9c5b2d13a7da9c8c0d8d578efad96e0fc59
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:02:36 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/17_vowel_cipher.py
@@ -0,0 +1,11 @@
+"""
+Write a method vowel_cipher that takes in a string and returns a new string where every vowel becomes the next vowel in the alphabet.
+"""
+
+
+def vowel_cipher(string):
+    pass
+
+
+print(vowel_cipher("bootcamp"))  # => buutcemp
+print(vowel_cipher("paper cup"))  # => pepir cap
