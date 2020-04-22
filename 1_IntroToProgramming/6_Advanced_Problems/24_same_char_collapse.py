ce00ca849f45d88c4279d1d0b9e084c06ffefb1d
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:27:34 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/24_same_char_collapse.py
@@ -0,0 +1,15 @@
+"""
+Write a method same_char_collapse that takes in a string and returns a collapsed version of the string. To collapse the string, we repeatedly delete 2 adjacent characters that are the same until there are no such adjacent characters. If there are multiple pairs that can be collapsed, delete the leftmost pair. For example, we take the following steps to collapse "zzzxaaxy": zzzxaaxy -> zxaaxy -> zxxy -> zy
+"""
+
+
+def same_char_collapse(string):
+    pass
+
+
+print(same_char_collapse("zzzxaaxy"))  # => "zy"
+# because zzzxaaxy -> zxaaxy -> zxxy -> zy
+
+
+print(same_char_collapse("uqrssrqvtt"))  # => "uv"
+# because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
