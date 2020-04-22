72171acd0b3e8e7f32bcbfc875e4ec0f8ca7fb3f
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 11:09:26 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/4_whisper_words.py
@@ -0,0 +1,11 @@
+"""
+Write a method whisper_words that takes in an array of words and returns a new array containing a whispered version of each word. See the examples. Solve this using map :-).
+"""
+
+
+def whisper_words(words):
+    pass
+
+
+# => ["keep...", "the...", "noise...", "down..."]
+print(whisper_words(["KEEP", "The", "NOISE", "down"]))
