40e8e0729efa5e9c989f458d3557550d8997176b
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 15:05:49 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/18_double_letter_count.py
@@ -0,0 +1,11 @@
+"""
+Write a method that takes in a string and returns the number of times that the same letter repeats twice in a row.
+"""
+
+
+def double_letter_count(string):
+    pass
+
+
+print(double_letter_count("the jeep rolled down the hill"))  # => 3
+print(double_letter_count("bootcamp"))  # => 1
