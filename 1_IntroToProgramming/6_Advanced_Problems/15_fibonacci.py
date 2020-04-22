51c0d13b926574deec7c8483ecd719118da223b7
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:54:14 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/15_fibonacci.py
@@ -0,0 +1,3 @@
+"""
+The fibonacci sequence is a sequence of numbers whose first and second elements are 1. To generate further elements of the sequence we take the sum of the previous two elements. For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length.
+"""
