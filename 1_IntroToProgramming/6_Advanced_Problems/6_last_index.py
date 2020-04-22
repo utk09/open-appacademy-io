41768eee0f7db61e28774c4987d8591222820354
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:31:19 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/6_last_index.py
@@ -0,0 +1,13 @@
+"""
+Write a method last_index that takes in a string and a character. The method should return the last index where the character can be found in the string.
+"""
+
+
+def last_index(string, character):
+    pass
+
+
+print(last_index("abca", "a"))  # => 3
+print(last_index("mississipi", "i"))  # => 9
+print(last_index("octagon", "o"))  # => 5
+print(last_index("programming", "m"))  # => 7
