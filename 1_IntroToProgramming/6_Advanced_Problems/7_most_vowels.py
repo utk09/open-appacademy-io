ec471211c4320c2951f63006524ce9dce69c6604
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-22 14:35:59 +0530

    reformatted

---
+++ 1_IntroToProgramming/6_Advanced_Problems/7_most_vowels.py
@@ -0,0 +1,9 @@
+"""
+Write a method most_vowels that takes in a sentence string and returns the word of the sentence that contains the most vowels.
+"""
+
+
+def most_vowels(sentence):
+    pass
+
+print(most_vowels("what a wonderful life"))  # => "wonderful"
