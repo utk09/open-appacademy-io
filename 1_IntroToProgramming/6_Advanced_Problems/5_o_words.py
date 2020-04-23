e7b16f07ad61db6c65c3d2ac36894b9372c3bab3
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-23 11:45:57 +0530

    added built in functions list

--- 1_IntroToProgramming/6_Advanced_Problems/5_o_words.py
+++ 1_IntroToProgramming/6_Advanced_Problems/5_o_words.py
@@ -2,6 +2,30 @@
 Write a method o_words that takes in a sentence string and returns an array of the words that contain an "o". Use select in your solution!
 """
 
+"""
+
+Built-in Functions( https://docs.python.org/3.6/library/functions.html )
+
+abs() | dict() | help() | min() | setattr() | all() | dir() | hex() | next()
+
+slice() | any() | divmod() | id() | object() | sorted() | ascii() | enumerate()
+
+input() | oct() | staticmethod() | bin() | eval() | int() | open() | str()
+
+bool() | exec() | isinstance() | ord() | sum() | bytearray() | filter()
+
+issubclass() | pow() | super() | bytes() | float() | iter() | print() | tuple()
+
+callable() | format() | len() | property() | type() | chr() | frozenset()
+
+list() | range() | vars() | classmethod() | getattr() | locals() | repr() | zip()
+
+compile() | globals() | map() | reversed() | __import__() | complex() | hasattr()
+
+max() | round() | delattr() | hash() | memoryview() | set()
+
+"""
+
 
 def o_words(sentence):
     pass
