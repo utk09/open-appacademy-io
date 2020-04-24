6f97994bd48578c6f634f7154be0a80e8b8ffb2e
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-24 18:59:23 +0530

    added pick primes

--- 1_IntroToProgramming/6_Advanced_Problems/9_pick_primes.py
+++ 1_IntroToProgramming/6_Advanced_Problems/9_pick_primes.py
@@ -4,7 +4,33 @@ Write a method pick_primes that takes in an array of numbers and returns a new a
 
 
 def pick_primes(numbers):
-    pass
+    prime_list = []
+    for each_num in numbers:
+        x = prime_or_not(each_num)
+        if x != None:
+            prime_list.append(x)
+    return prime_list
+
+
+def prime_or_not(new_num):
+    prime_list = []
+    last_num = new_num + 1
+
+    if new_num == 2:
+        return new_num
+
+    elif new_num > 2:
+        for each_num in range(2, last_num):
+            if new_num % each_num == 0:
+                prime_list.append(each_num)
+        if len(prime_list) == 1:
+            for each_element in prime_list:
+                return each_element
+        else:
+            return None
+
+    else:
+        return None
 
 
 print(pick_primes([2, 3, 4, 5, 6]))  # => [2, 3, 5]
