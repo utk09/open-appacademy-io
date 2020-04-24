80daf3a74d3a6cf6fb6494ea5e98873cc70ea9f2
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-24 13:40:02 +0530

    added prime program

--- 1_IntroToProgramming/6_Advanced_Problems/8_prime.py
+++ 1_IntroToProgramming/6_Advanced_Problems/8_prime.py
@@ -4,7 +4,23 @@ Write a method prime_or_not that takes in a number and returns a boolean, indica
 
 
 def prime_or_not(num):
-    pass
+    prime_list = []
+    last_num = num + 1
+
+    if num == 2:
+        return True
+
+    elif num > 2:
+        for each_num in range(2, last_num):
+            if num % each_num == 0:
+                prime_list.append(each_num)
+        if len(prime_list) == 1:
+            return True
+        else:
+            return False
+
+    else:
+        return False
 
 
 print(prime_or_not(2))  # => true
