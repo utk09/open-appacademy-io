5bc44f3325c6ef3a8bf8c982fd5280be0786bce0
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-24 19:10:55 +0530

    added prime factors

--- 1_IntroToProgramming/6_Advanced_Problems/10_prime_factors.py
+++ 1_IntroToProgramming/6_Advanced_Problems/10_prime_factors.py
@@ -4,7 +4,43 @@ Write a method prime_factors that takes in a number and returns an array contain
 
 
 def prime_factors(number):
-    pass
+    final_list = []
+    prime_list_2 = pick_primes(number)
+    for each_value in prime_list_2:
+        if number % each_value == 0:
+            final_list.append(each_value)
+
+    return final_list
+
+
+def pick_primes(numbers):
+    prime_list = []
+    for each_num in range(numbers):
+        x = prime_or_not(each_num)
+        if x != None:
+            prime_list.append(x)
+    return prime_list
+
+
+def prime_or_not(new_num):
+    prime_list = []
+    last_num = new_num + 1
+
+    if new_num == 2:
+        return new_num
+
+    elif new_num > 2:
+        for each_num in range(2, last_num):
+            if new_num % each_num == 0:
+                prime_list.append(each_num)
+        if len(prime_list) == 1:
+            for each_element in prime_list:
+                return each_element
+        else:
+            return None
+
+    else:
+        return None
 
 
 print(prime_factors(24))  # => [2, 3]
