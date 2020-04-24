f047b88bf6744541172a8dd9bcd46ebe81da6316
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-24 10:52:31 +0530

    added most vowels program

--- 1_IntroToProgramming/6_Advanced_Problems/7_most_vowels.py
+++ 1_IntroToProgramming/6_Advanced_Problems/7_most_vowels.py
@@ -4,6 +4,25 @@ Write a method most_vowels that takes in a sentence string and returns the word 
 
 
 def most_vowels(sentence):
-    pass
+    vowels = "aeiou"
+    word_dict = {}
+    count = 0
+    split_sentence = sentence.split(" ")
+    for each_word in split_sentence:
+        for each_letter in each_word:
+            if each_letter in vowels:
+                count += 1
+        word_dict[each_word] = count
+        count = 0
+
+    value_list = []
+    for key, value in word_dict.items():
+        value_list.append(value)
+        max_value = max(value_list)
+
+    for key, value in word_dict.items():
+        if value == max_value:
+            return key
+
 
 print(most_vowels("what a wonderful life"))  # => "wonderful"
