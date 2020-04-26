09a72542e807545b3a6fa503f12d594cca75fd6e
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-26 09:07:47 +0530

    added method 2 to caesar cipher

--- 1_IntroToProgramming/6_Advanced_Problems/16_caesar_cipher.py
+++ 1_IntroToProgramming/6_Advanced_Problems/16_caesar_cipher.py
@@ -6,18 +6,37 @@ Write a method caesar_cipher that takes in a string and a number. The method sho
 # alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
-# chr(97) = a
-# ord('a') = 97
-
 def caesar_cipher(string, num):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     string = string.lower()
     new_str = ""
-    for character in range(len(string)):
-        char_new = string[character]
-        new_str = new_str + chr((ord(char_new) + num - 97) % 26 + 97)
+    for each_char in string:
+        if each_char in alphabet:
+            old_pos = alphabet.index(each_char)
+            new_pos = old_pos + num
+            new_str = new_str + alphabet[new_pos % 26]
     return new_str
 
 
 print(caesar_cipher("apple", 1))  # => "bqqmf"
 print(caesar_cipher("bootcamp", 2))  # => "dqqvecor"
 print(caesar_cipher("zebra", 4))  # => "difve"
+
+
+# Caesar Cipher Method 2
+
+# chr(97) = a
+# ord('a') = 97
+
+
+# def caesar_cipher(string, num):
+#     string = string.lower()
+#     new_str = ""
+#     for character in range(len(string)):
+#         char_new = string[character]
+#         new_str = new_str + chr((ord(char_new) + num - 97) % 26 + 97)
+#     return new_str
+
+# print(caesar_cipher("apple", 1))  # => "bqqmf"
+# print(caesar_cipher("bootcamp", 2))  # => "dqqvecor"
+# print(caesar_cipher("zebra", 4))  # => "difve"
--- 1_IntroToProgramming/6_Advanced_Problems/17_vowel_cipher.py
+++ 1_IntroToProgramming/6_Advanced_Problems/17_vowel_cipher.py
@@ -4,7 +4,12 @@ Write a method vowel_cipher that takes in a string and returns a new string wher
 
 
 def vowel_cipher(string):
-    pass
+    vowel = "aeiou"
+    new_str = ""
+    for each_char in string:
+        if each_char in vowel:
+            
+
 
 
 print(vowel_cipher("bootcamp"))  # => buutcemp
