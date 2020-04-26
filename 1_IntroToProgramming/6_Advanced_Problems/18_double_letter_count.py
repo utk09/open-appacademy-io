7a75e4f77684d1b4ab96156e234decb478188324
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-26 10:34:02 +0530

    added double letter

--- 1_IntroToProgramming/6_Advanced_Problems/18_double_letter_count.py
+++ 1_IntroToProgramming/6_Advanced_Problems/18_double_letter_count.py
@@ -4,7 +4,13 @@ Write a method that takes in a string and returns the number of times that the s
 
 
 def double_letter_count(string):
-    pass
+    new_str = string.split(" ")
+    count = 0
+    for each_word in new_str:
+        for i in range(len(each_word) - 1):
+            if each_word[i] == each_word[i + 1]:
+                count += 1
+    return count
 
 
 print(double_letter_count("the jeep rolled down the hill"))  # => 3
