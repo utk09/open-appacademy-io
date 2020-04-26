b2e1a1856ee814ca2dd80a9edad06c7cede36255
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-26 15:01:51 +0530

    added consonant cancel

--- 1_IntroToProgramming/6_Advanced_Problems/23_consonant_cancel.py
+++ 1_IntroToProgramming/6_Advanced_Problems/23_consonant_cancel.py
@@ -4,7 +4,23 @@ Write a method consonant_cancel that takes in a sentence and returns a new sente
 
 
 def consonant_cancel(sentence):
-    pass
+    sentence = sentence.split(" ")
+    sent_arr = []
+    final_sent = " "
+    for each_word in sentence:
+        sent_arr.append(minus_conso(each_word))
+
+    final_sent = " ".join(map(str, sent_arr))
+    return final_sent
+
+
+def minus_conso(word):
+    vowels = "aeiou"
+    for each_index, each_char in enumerate(word):
+        if each_char in vowels:
+            after_vowel = word[each_index:]
+            new_word = after_vowel
+            return new_word
 
 
 print(consonant_cancel("down the rabbit hole"))  # => "own e abbit ole"
