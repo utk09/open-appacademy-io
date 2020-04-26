2cbfd89d0c84e4979aa71ca17265ed2a2aef0cdd
utkarsh <utkarsh.tiwari@somaiya.edu>
2020-04-26 09:12:19 +0530

    added vowel cipher

--- 1_IntroToProgramming/6_Advanced_Problems/17_vowel_cipher.py
+++ 1_IntroToProgramming/6_Advanced_Problems/17_vowel_cipher.py
@@ -8,9 +8,14 @@ def vowel_cipher(string):
     new_str = ""
     for each_char in string:
         if each_char in vowel:
-            
-
+            old_pos = vowel.index(each_char)
+            new_pos = old_pos + 1
+            new_str = new_str + vowel[new_pos % len(vowel)]
+        else:
+            new_str = new_str + each_char
+    return new_str
 
 
 print(vowel_cipher("bootcamp"))  # => buutcemp
 print(vowel_cipher("paper cup"))  # => pepir cap
+print(vowel_cipher("aeiou")) # => eioua
